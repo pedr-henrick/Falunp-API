0a5dc5fea5cef10dbe3d34495bf449d0c7784459
pedr-henrick <pedro.mesquita.hc@gmail.com>
2025-09-15 13:52:53 -0300

    feat: adiciona quantidade de alunos matriculados nas turmas

---
+++ src/Application/Mappings/MapsterConfig.cs
@@ -0,0 +1,16 @@
+﻿using Application.DTOs.Class;
+using Domain.Entities;
+using Mapster;
+
+namespace Application.Mappings
+{
+    public static class MapsterConfig
+    {
+        public static void RegisterMappings()
+        {
+            TypeAdapterConfig<Class, ClassInfoDto>
+                .NewConfig()
+                .Map(dest => dest.StudentCount, src => src.Enrollments.Count);
+        }
+    }
+}
